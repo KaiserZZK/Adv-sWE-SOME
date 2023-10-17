fa49b6d6c6566d33eabdb3aed5f2c1dcb711f008
zekaizhang <zz2919@columbia.edu>
2023-10-17 13:33:41 -0400

    wip: Update Profile class to save non-id fields to db

--- src/main/java/com/advswesome/advswesome/repository/document/Profile.java
+++ src/main/java/com/advswesome/advswesome/repository/document/Profile.java
@@ -28,6 +28,21 @@ public class Profile {
         this.profileId = profileId;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 
     public static class MedicalHistory {
         private String diseaseName;
