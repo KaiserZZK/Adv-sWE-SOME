33db3c4ec262a93e399a0b7def23eb6d725d2d61
zhizhen <zzz1281063003@gmail.com>
2023-10-18 17:10:43 -0400

    add controller test for rx

--- src/test/java/com/advswesome/advswesome/controller/PrescriptionControllerTest.java
+++ src/test/java/com/advswesome/advswesome/controller/PrescriptionControllerTest.java
@@ -1,6 +1,5 @@
 package com.advswesome.advswesome.controller;
 
-import com.advswesome.advswesome.repository.PrescriptionRepository;
 import com.advswesome.advswesome.repository.document.Prescription;
 import com.advswesome.advswesome.service.PrescriptionService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,29 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import reactor.core.publisher.Mono;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
 public class PrescriptionControllerTest {
 
-    @Autowired
-    private WebApplicationContext context;
 
-    private MockMvc mockMvc;
     @InjectMocks
     private PrescriptionController prescriptionController;
 
@@ -39,35 +21,78 @@ public class PrescriptionControllerTest {
 
     @BeforeEach
     void setup() {
-//        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();;
+        MockitoAnnotations.openMocks(this);
     }
 
-//    @Test
-//    void testCreatRx() throws Exception {
-//        Prescription p = new Prescription();
-//        p.setRx_number("12345");
-//        p.setRx_provider("ProviderA");
-//        p.setRx_name("MedicineA");
-//        p.setRefills(2);
-//        p.setQuantity(30);
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        String json = mapper.writeValueAsString(p);
-//
-//
-//        when(prescriptionService.createPrescription(p)).thenReturn(Mono.just(p));
-//
-//        mockMvc.perform(post("/prescriptions")
-//                .contentType("application/json")
-//                .content(json))
-//                .andExpect(status().isOk())
-//                        .andDo(print());
-////                .andExpect(jsonPath("$.rx_number").value("12345"));
-//
-////        System.out.println(result.getResponse().getContentAsString());
-//
-//        verify(prescriptionService, times(1)).createPrescription(any(Prescription.class));
-//
-//    }
+    @Test
+    void testCreatRx() throws Exception {
+        Prescription p = new Prescription();
+        p.setRx_number("12345");
+        p.setRx_provider("ProviderA");
+        p.setRx_name("MedicineA");
+        p.setRefills(2);
+        p.setQuantity(30);
+
+        when(prescriptionService.createPrescription(p)).thenReturn(Mono.just(p));
+
+        prescriptionController.createPrescription(p).block();
+
+        verify(prescriptionService, times(1)).createPrescription(p);
+
+    }
+
+
+    @Test
+    void testGetbyRxId(){
+        Prescription p = new Prescription();
+        p.setRx_number("12345");
+        p.setProfile_id("pp");
+        p.setRx_provider("ProviderA");
+        p.setRx_name("MedicineA");
+        p.setRefills(2);
+        p.setQuantity(30);
+
+        when(prescriptionService.getPrescriptionById("12345")).thenReturn(Mono.just(p));
+        prescriptionController.getPrescriptionById("12345").block();
+        verify(prescriptionService, times(1)).getPrescriptionById("12345");
+
+    }
+
+
+    @Test
+    void testUpdatebyRxId(){
+        Prescription p = new Prescription();
+        p.setRx_number("12345");
+        p.setProfile_id("pp");
+        p.setRx_provider("ProviderA");
+        p.setRx_name("MedicineA");
+        p.setRefills(2);
+        p.setQuantity(30);
+
+        when(prescriptionService.getPrescriptionById("12345")).thenReturn(Mono.just(p));
+        when(prescriptionService.updatePrescription(p)).thenReturn(Mono.just(p));
+
+        prescriptionController.updatePrescription("12345",p).block();
+        verify(prescriptionService, times(1)).getPrescriptionById("12345");
+
+    }
+
+    @Test
+    void testDeleteRx() {
+        Prescription p = new Prescription();
+        p.setRx_number("12345");
+        p.setProfile_id("pp");
+        p.setRx_provider("ProviderA");
+        p.setRx_name("MedicineA");
+        p.setRefills(2);
+        p.setQuantity(10);
+
+        when(prescriptionService.getPrescriptionById("12345")).thenReturn(Mono.just(p));
+        when(prescriptionService.deletePrescription("12345")).thenReturn(Mono.empty());
+
+        prescriptionController.deletePrescription("12345").block();
+
+
+        verify(prescriptionService, times(1)).deletePrescription("12345");
+    }
 }
