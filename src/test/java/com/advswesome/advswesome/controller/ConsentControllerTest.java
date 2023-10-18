54aba22fc35924f31a95c7c84a578bcaaa6ea4e9
Feiyang Zhu <fzhu@email.wm.edu>
2023-10-18 18:51:43 -0400

    add comments and test case for get consent by profile id

--- src/test/java/com/advswesome/advswesome/controller/ConsentControllerTest.java
+++ src/test/java/com/advswesome/advswesome/controller/ConsentControllerTest.java
@@ -29,55 +29,71 @@ public class ConsentControllerTest {
     }
 
     @Test
-    void testCreateConsent() throws Exception {
-
+    void testCreateConsent() {
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
+        // when createConsent called, return the same consent
         when(consentService.createConsent(consent)).thenReturn(Mono.just(consent));
+
+        // createConsent from the controller
         consentController.createConsent(consent).block();
 
+        // verify the createConsent method from consentService only called once
         verify(consentService, times(1)).createConsent(consent);
-
     }
 
 
     @Test
-    void testGetbyConsentId(){
-
+    void testGetByConsentId(){
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
+        // when getConsentById called, return the same consent
         when(consentService.getConsentById("123")).thenReturn(Mono.just(consent));
-        consentController.getConsentById("123").block();
-        verify(consentService, times(1)).getConsentById("123");
 
+        // getConsentById from the controller
+        consentController.getConsentById("123").block();
+
+        // verify the getConsentById method from consentService only called once
+        verify(consentService, times(1)).getConsentById("123");
     }
 
 
     @Test
     void testUpdateConsent(){
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
+        // when getConsentById & updateConsent called, return the same consent
         when(consentService.getConsentById("123")).thenReturn(Mono.just(consent));
         when(consentService.updateConsent(consent)).thenReturn(Mono.just(consent));
 
+        // updateConsent from the controller
         consentController.updateConsent("123", consent).block();
-        verify(consentService, times(1)).getConsentById("123");
 
+        // verify the updateConsent method from consentService only called once
+        verify(consentService, times(1)).updateConsent(consent);
     }
 
     @Test
     void testDeleteConsent() {
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
+        // when getConsentById & deleteConsent called, return the same consent
         when(consentService.getConsentById("123")).thenReturn(Mono.just(consent));
         when(consentService.deleteConsent("123")).thenReturn(Mono.empty());
 
+        // deleteConsent from the controller
         consentController.deleteConsent("123").block();
 
+        // verify the deleteConsent method from consentService only called once
         verify(consentService, times(1)).deleteConsent("123");
     }
+
 }
--- src/test/java/com/advswesome/advswesome/service/ConsentServiceTest.java
+++ src/test/java/com/advswesome/advswesome/service/ConsentServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import reactor.core.publisher.Mono;
+
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,63 +28,98 @@ public class ConsentServiceTest {
 
     @Test
     void testCreateConsent() {
-
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
+        // when save method is called, use Mockito to return a Mono of consent
         when(consentRepository.save(consent)).thenReturn(Mono.just(consent));
         Consent savedConsent = consentService.createConsent(consent).block();
 
+        // check consent content
         assertNotNull(savedConsent);
         assertEquals(consent.getConsentId(), savedConsent.getConsentId());
         assertEquals(consent.getProfileId(), savedConsent.getProfileId());
         assertEquals(consent.isPermission(), savedConsent.isPermission());
         assertEquals(consent.getUpdatedAt(), savedConsent.getUpdatedAt());
 
+        // verify the save method is called exactly once
         verify(consentRepository, times(1)).save(consent);
     }
 
     @Test
     void testGetConsentByID(){
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
+        // when findById method is called, use Mockito to return a Mono of consent
         when(consentRepository.findById("123")).thenReturn(Mono.just(consent));
-
         Consent fetchedConsent = consentService.getConsentById("123").block();
+
+        // check consent content
         assertNotNull(fetchedConsent);
         assertEquals(consent.getConsentId(), fetchedConsent.getConsentId());
         assertEquals(consent.getProfileId(), fetchedConsent.getProfileId());
         assertEquals(consent.isPermission(), fetchedConsent.isPermission());
         assertEquals(consent.getUpdatedAt(), fetchedConsent.getUpdatedAt());
+
+        // verify the findById method is called exactly once
+        verify(consentRepository, times(1)).findById("123");
     }
 
 
     @Test
     void testUpdate(){
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
-        when(consentRepository.findById("123")).thenReturn(Mono.just(consent));
-        when(consentRepository.save(any(Consent.class))).thenReturn(Mono.just(consent));
-
+        // save and then update consent
+        when(consentRepository.save(consent)).thenReturn(Mono.just(consent));
         consent.setPermission(false);
+
+        // block the return value of the updated consent
         Consent fetchedConsent = consentService.updateConsent(consent).block();
+
+        // check consent content
         assertNotNull(fetchedConsent);
         assertFalse(fetchedConsent.isPermission());
     }
 
     @Test
-    void testDeleteRx() {
+    void testDeleteConsent() {
+        // create a new consent
         Date date = new Date();
         Consent consent = new Consent("123", "A2", true, date);
 
-        when(consentRepository.findById("123")).thenReturn(Mono.just(consent));
+        // when deleteById method is called, use Mockito to return a Mono of consent
         when(consentRepository.deleteById("123")).thenReturn(Mono.empty());
-
         consentService.deleteConsent("123").block();
 
+        // verify the deleteById method is called exactly once
         verify(consentRepository, times(1)).deleteById("123");
+    }
+
+    @Test
+    void testGetConsentByProfileId() {
+        // create a new consent
+        Date date = new Date();
+        Consent consent = new Consent("123", "A2", true, date);
+
+        // when findByProfileId method is called, use Mockito to return a Mono of consent
+        when(consentRepository.findByProfileId("A2")).thenReturn(Mono.just(consent));
+        Consent fetchedConsent = consentService.getConsentByProfileId("A2").block();
+
+        // check consent content
+        assertNotNull(fetchedConsent);
+        assertEquals(consent.getConsentId(), fetchedConsent.getConsentId());
+        assertEquals(consent.getProfileId(), fetchedConsent.getProfileId());
+        assertEquals(consent.isPermission(), fetchedConsent.isPermission());
+        assertEquals(consent.getUpdatedAt(), fetchedConsent.getUpdatedAt());
+
+        // verify the findByProfileId method is called exactly once
+        verify(consentRepository, times(1)).findByProfileId("A2");
     }
 
 }
