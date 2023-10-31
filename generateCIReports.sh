a3f0feb60f2b495182dec727122d34c04c120b80
zekaizhang <zz2919@columbia.edu>
2023-10-31 13:29:20 -0400

    Fix CI report generation
    - remove report-generation shell from CI script
    - included reports for build, unit test, and style checker
    - completed status lines in report-generation shell

--- generateCIReports.sh
+++ generateCIReports.sh
@@ -2,12 +2,14 @@ echo "Generating CI reports..."
 
 echo "Building project and logging..."
 ./mvnw clean install >> ./reports/build-report.txt
+echo "✅ (1/3) Generated build report at ./reports/build-report.txt"
 
-
+echo "Running unit tests and logging..."
 ./mvnw test >> ./reports/unittest-report.txt
+echo "✅ (2/3) Generated unit test report at ./reports/unittest-report.txt"
 
-
+echo "Running style checker and logging..."
 ./mvnw checkstyle:check >> ./reports/checkstyle-report.txt
+echo "✅ (3/3) Generated style checker report at ./reports/checkstyle-report.txt"
 
-
-echo "Job done!"
+echo "🌸 Job done!"
