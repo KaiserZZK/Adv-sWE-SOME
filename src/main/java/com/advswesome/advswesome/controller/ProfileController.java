2efd21a103d4068a8c705ec084ba60453a62c88d
zekaizhang <zz2919@columbia.edu>
2023-12-03 09:53:22 -0500

    Refactor profile-related endpoints for authorization to work

--- src/main/java/com/advswesome/advswesome/controller/ProfileController.java
+++ src/main/java/com/advswesome/advswesome/controller/ProfileController.java
@@ -23,36 +23,51 @@ public class ProfileController {
     }
 
     @PostMapping
-    public Mono<Profile> createProfile(@AuthenticationPrincipal UserPrincipal principal, @RequestBody Profile profile) {
-        return profileService.createProfile(profile);
+    public ResponseEntity<Profile> createProfile(
+        @AuthenticationPrincipal UserPrincipal principal, 
+        @RequestBody Profile profile) 
+    {
+        Mono<Profile> profileMono = profileService.createProfile(profile);
+        Profile newProfile  = profileMono.block();
+        return ResponseEntity.status(HttpStatus.OK).body(newProfile);
     }
 
     @GetMapping("/{profileId}")
-    public Mono<Profile> getProfileById(@PathVariable String profileId) {
-        return profileService.getProfileById(profileId);
+    public ResponseEntity<Profile> getProfileById(
+        @AuthenticationPrincipal UserPrincipal principal, 
+        @PathVariable String profileId) 
+    {
+        Mono<Profile> profileMono = profileService.getProfileById(profileId);
+        Profile foundProfile  = profileMono.block();
+        return ResponseEntity.status(HttpStatus.OK).body(foundProfile);
     }
 
     @PutMapping("/{profileId}")
-    public Mono<ResponseEntity<Profile>> updateProfile(@PathVariable String profileId, @RequestBody Profile profile) {
-        return profileService.getProfileById(profileId)
-                .flatMap(existing -> {
-                    profile.setProfileId(profileId);
-                    return profileService.updateProfile(profile);
-                })
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+    public ResponseEntity<Profile> updateProfile(
+        @AuthenticationPrincipal UserPrincipal principal, 
+        @PathVariable String profileId, 
+        @RequestBody Profile profile) 
+    {
+        Mono<Profile> profileMono = profileService.updateProfile(profile);
+        Profile updatedProfile  = profileMono.block();
+        return ResponseEntity.status(HttpStatus.OK).body(updatedProfile);
     }
 
     @DeleteMapping("/{profileId}")
-    public Mono<ResponseEntity<Void>> deleteProfile(@PathVariable String profileId) {
-        return profileService.getProfileById(profileId)
-                .flatMap(existing -> profileService.deleteProfile(profileId).thenReturn(existing))
-                .map(profile -> ResponseEntity.noContent().<Void>build())
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+    public ResponseEntity<Void> deleteProfile(
+        @AuthenticationPrincipal UserPrincipal principal, 
+        @PathVariable String profileId
+    ) {
+        Mono<Void> profileMono = profileService.deleteProfile(profileId);
+        var deletedProfile = profileMono.block();
+        return ResponseEntity.status(HttpStatus.OK).body(deletedProfile);
     }
 
     @GetMapping("/user/{userId}")
-    public ResponseEntity<List<Profile>> getProfilesByUserId(@AuthenticationPrincipal UserPrincipal principal, @PathVariable String userId) {
+    public ResponseEntity<List<Profile>> getProfilesByUserId(
+        @AuthenticationPrincipal UserPrincipal principal, 
+        @PathVariable String userId) 
+    {
         Flux<Profile> profilesFlux = profileService.getProfilesByUserId(userId);
         List<Profile> profilesList = profilesFlux.collectList().block();
         return ResponseEntity.status(HttpStatus.OK).body(profilesList);
--- src/main/java/com/advswesome/advswesome/security/WebSecurityConfiguration.java
+++ src/main/java/com/advswesome/advswesome/security/WebSecurityConfiguration.java
@@ -13,10 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-// import static org.springframework.http.HttpMethod.DELETE;
-// import static org.springframework.http.HttpMethod.GET;
-// import static org.springframework.http.HttpMethod.POST;
-// import static org.springframework.http.HttpMethod.PUT;
 
 @Configuration
 @EnableWebSecurity
